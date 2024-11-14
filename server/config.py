6a4e955951273be95d140d3656d18e1a20729ad2
grinnsi <anton.may@gmx.net>
2024-11-14 12:37:22 +0100

    Set color and format of console logger

--- server/config.py
+++ server/config.py
@@ -1,3 +1,6 @@
+from logging.config import dictConfig
+import coloredlogs
+
 # Class as namespace for arguments (for autocomplete and more)
 class Arguments(object):
     HELP = False
@@ -9,4 +12,44 @@ class Arguments(object):
 class Config(object):
     @staticmethod
     def init_logger(debug_mode: bool):
-        pass
+        coloredlogs.DEFAULT_LEVEL_STYLES = dict(
+            spam=dict(color=22, faint=True),
+            debug=dict(color=28),
+            verbose=dict(color=34),
+            info=dict(),
+            notice=dict(color=220),
+            warning=dict(color=202),
+            success=dict(color=118, bold=True),
+            error=dict(color=124, bold=True),
+            critical=dict(color=196, background='white', inverse=True, bright=True),
+        )
+        coloredlogs.DEFAULT_FIELD_STYLES = dict(
+            asctime=dict(color='cyan'),
+            hostname=dict(color='magenta'),
+            levelname=dict(color=13, bold=True),
+            name=dict(color='blue'),
+            programname=dict(color='cyan'),
+            username=dict(color='yellow'),
+        )
+        dictConfig({
+            'version': 1,
+            'formatters': {
+                'default': {
+                    '()': 'coloredlogs.ColoredFormatter',
+                    'format': '[%(asctime)s] %(levelname)-8s in %(module)-20s %(message)s',
+                    'datefmt': '%H:%M:%S, %d-%m-%Y'
+                }
+            },
+            'handlers': {
+                'wsgi': {
+                    'class': 'logging.StreamHandler',
+                    'stream': 'ext://flask.logging.wsgi_errors_stream',
+                    'formatter': 'default',
+                }
+            },
+            'root': {
+                # Configure severity level
+                'level': 'DEBUG' if debug_mode else "WARNING",
+                'handlers': ['wsgi']
+            },
+        })
