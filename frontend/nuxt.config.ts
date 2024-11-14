1eb452dcba253a85424a2839ff6db9244f99dbb9
grinnsi <anton.may@gmx.net>
2024-11-14 23:05:41 +0100

    Updated nuxt configs

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-11-13',
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: 'Interface',
+    },
+  },
+  modules: [
+    ['@nuxt/eslint', { fix: true }],
+    '@element-plus/nuxt',
+  ],
+  elementPlus: {
+    importStyle: 'scss',
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/scss/theming.scss" as *;',
+        },
+      },
+    },
+  },
+  vue: {
+    compilerOptions: {
+      isCustomElement: (tag) => tag === 'iconify-icon',
+    },
+  },
+  nitro: {
+    preset: 'static',
+  }
 })
