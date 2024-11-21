bd9a864fff6e430efb2ae6023bae891593f414d8
grinnsi <anton.may@gmx.net>
2024-11-21 09:35:17 +0100

    Updated connections endpoint to directly read JSON

--- server/web/settings/routes.py
+++ server/web/settings/routes.py
@@ -1,21 +1,22 @@
 from logging import getLogger
 from flask import Blueprint, request, Response
 
-from .connections import get_connections
+from .connections import create_new_connection, get_connections
 
 def create_data_endpoints() -> Blueprint:
     bp = Blueprint("database_endpoints", __name__, url_prefix="/data")
     
     @bp.route('/connections', methods=("GET", "POST" ,"DELETE"))
     def connections() -> Response:
+        request_data = request.get_json()
         logger = getLogger()
-        logger.debug(msg=f"Received following connections request: {request.method} {request.data}")
+        logger.debug(msg=f"Received following connections request: {request.method} {request_data}")
 
         if request.method == "GET":
             return get_connections()
         
         if request.method == "POST":
-            pass
+            create_new_connection(request_data)
         
         if request.method == "DELETE":
             pass
