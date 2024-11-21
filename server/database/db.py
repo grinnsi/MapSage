2f6eaaac52ec73a12263b70e6f124a0422449c51
grinnsi <anton.may@gmx.net>
2024-11-21 09:34:41 +0100

    Updated 'psycopg2' requirement to 'psycopg[binary,pool]; Updated 'test_pg_connection' method

--- server/database/db.py
+++ server/database/db.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-from sqlalchemy import Engine
+from sqlalchemy import Engine, text
 from sqlmodel import SQLModel, Session, create_engine, delete, select 
 
 from .models import Connection, Namespace
@@ -41,13 +41,13 @@ class Database():
 
     # Input connection parameters and returns if connection is successful
     @classmethod
-    def test_pg_connection(cls, data: Connection) -> bool:
+    def test_pg_connection(cls, data: dict) -> bool:
         """Test the connection to a PostgreSQL database, using the connection parameters"""
-        pg_url = f"postgresql+psycopg2://{data.name}:{data.password}@{data.host}:{data.port}/{data.database_name}"
+        pg_url = f"postgresql+psycopg://{data["role"]}:{data["password"]}@{data["host"]}:{data["port"]}/{data["database_name"]}"
         pg_engine = create_engine(pg_url, echo=cls.debug_mode)
         
         with Session(pg_engine) as session:
-            stmt = select("SELECT * FROM information_schema.tables")
+            stmt = select(text(" * FROM information_schema.tables"))
             result = session.exec(stmt)
             cls.logger.debug(msg=f"Result of PostgreSQL connection test: {result}")
             
