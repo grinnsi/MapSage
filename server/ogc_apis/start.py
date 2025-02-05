6ddc2a7c5b4302c033f4b3b00553a3d544e658bb
grinnsi <anton.may@gmx.net>
2025-02-05 19:51:48 +0100

    Updated generated files

--- server/ogc_apis/start.py
+++ server/ogc_apis/start.py
@@ -6,12 +6,12 @@ from server.config import get_logger_config
 
 def start_api_server(debug_mode: bool) -> uvicorn.Server:
     config = uvicorn.Config(
-        app=features_api.init_app(),
+        app=features_api.init_api_server(),
         port=int(os.getenv("APISERVER_PORT", "8000")),
         root_path=os.getenv("APISERVER_ROOT_PATH", ""),
         proxy_headers=True,
         log_config=get_logger_config(debug_mode),
         log_level="debug" if debug_mode else "warning",
-        reload=debug_mode,
+        reload=True,
     )
     return uvicorn.Server(config)
