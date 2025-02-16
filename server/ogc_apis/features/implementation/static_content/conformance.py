0fbc825130206483285c66e85482fdc3d3c5c496
grinnsi <anton.may@gmx.net>
2025-02-16 22:28:38 +0100

    Add generate_conformance function to return compliance classes for OGC API features

---
+++ server/ogc_apis/features/implementation/static_content/conformance.py
@@ -0,0 +1,14 @@
+from server.ogc_apis.features.models.conf_classes import ConfClasses
+
+def generate_conformance() -> ConfClasses:
+    conforms_to = [
+        "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core",
+        "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/oas30",
+        "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/html",
+        "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/geojson",
+        "http://www.opengis.net/spec/ogcapi-features-2/1.0/conf/crs",
+    ]
+    
+    conf_classes = ConfClasses(conforms_to=conforms_to)
+    
+    return conf_classes
