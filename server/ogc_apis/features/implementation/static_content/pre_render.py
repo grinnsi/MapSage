9033b6f816a056b69de3f37d4bfb82117652f8b7
grinnsi <anton.may@gmx.net>
2025-02-20 22:39:03 +0100

    Decode JSON output in generate_link function for consistent string return type

--- server/ogc_apis/features/implementation/static_content/pre_render.py
+++ server/ogc_apis/features/implementation/static_content/pre_render.py
@@ -2,11 +2,11 @@ import orjson
 
 def generate_link(obj: dict | list[dict]) -> str:
     if isinstance(obj, list):
-        return orjson.dumps([orjson.loads(generate_link(item)) for item in obj])
+        return orjson.dumps([orjson.loads(generate_link(item)) for item in obj]).decode("utf-8")
     
     return orjson.dumps({
         "href": obj['url'],
         "rel": obj['rel'],
         "type": obj['type'],
         "title": obj['title']
-    })
+    }).decode("utf-8")
