9cb0bed2acc265e59cfe96001b8f9864b80d26b8
grinnsi <anton.may@gmx.net>
2025-03-04 22:22:35 +0100

    Add fillSpace prop to TemplateSection to fill available space with main content

--- frontend/components/Template/Section.vue
+++ frontend/components/Template/Section.vue
@@ -1,12 +1,12 @@
 <template>
-  <section>
+  <section class="section" :class="{ 'fill-space': fillSpace }">
     <div class="section-header">
       <h4>{{ sectionTitle }}</h4>
       <div>
         <slot name="header-right"></slot>
       </div>
     </div>
-    <div class="section-content">
+    <div class="section-content" :class="{ 'fill-space': fillSpace }" :style="sectionContentStyle">
       <slot>ABSCHNITTINHALT</slot>
     </div>
     <div class="section-footer">
@@ -22,10 +22,26 @@ defineProps({
     required: true,
     default: "Titel"
   },
+  fillSpace: {
+    type: Boolean,
+    default: false
+  },
+  sectionContentStyle: {
+    type: String,
+  }
 })
 </script>
 
 <style scoped lang="scss">
+.section {
+  display: flex;
+  flex-direction: column;
+}
+
+.fill-space {
+  flex: 1;
+}
+
 .section-header {
   display: flex;
   flex-direction: row;
