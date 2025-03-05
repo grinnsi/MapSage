0a56bd8841139c7537922ba8f0c7988d85bf6608
grinnsi <anton.may@gmx.net>
2025-03-05 16:58:40 +0100

    Refactor spatial reference util function to combine main code in a shared function

--- server/utils/crs_identifier.py
+++ server/utils/crs_identifier.py
@@ -1,6 +1,6 @@
 from osgeo.osr import SpatialReference
 
-def get_uri_of_spatial_ref(spatial_ref: SpatialReference) -> str:
+def get_code_and_authority_of_spatial_ref(spatial_ref: SpatialReference) -> tuple[str, str]:
     if spatial_ref is None:
         raise ValueError("Spatial reference is None")
     
@@ -11,19 +11,15 @@ def get_uri_of_spatial_ref(spatial_ref: SpatialReference) -> str:
     code = spatial_ref.GetAuthorityCode(None)
     if code is None:
         raise ValueError("Spatial reference has no authority code")
+    
+    return code, authority
+
+def get_uri_of_spatial_ref(spatial_ref: SpatialReference) -> str:
+    code, authority = get_code_and_authority_of_spatial_ref(spatial_ref)
     
     return f"http://www.opengis.net/def/crs/{authority}/0/{code}"
 
 def get_urn_of_spatial_ref(spatial_ref: SpatialReference) -> str:
-    if spatial_ref is None:
-        raise ValueError("Spatial reference is None")
-    
-    authority = spatial_ref.GetAuthorityName(None)
-    if authority is None:
-        raise ValueError("Spatial reference has no authority name")
-    
-    code = spatial_ref.GetAuthorityCode(None)
-    if code is None:
-        raise ValueError("Spatial reference has no authority code")
+    code, authority = get_code_and_authority_of_spatial_ref(spatial_ref)
     
     return f"urn:ogc:def:crs:{authority}::{code}"
