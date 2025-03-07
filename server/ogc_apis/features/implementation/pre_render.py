44a2a0bf4590e80f7309581b0bf55605efc7c279
grinnsi <anton.may@gmx.net>
2025-03-07 18:57:15 +0100

    Refactor pre render of links to now return ordered dictionaries with key checking

--- server/ogc_apis/features/implementation/pre_render.py
+++ server/ogc_apis/features/implementation/pre_render.py
@@ -1,18 +1,37 @@
+import re
 import orjson
 
-def generate_link(obj: dict) -> str:
+def generate_link(obj: dict) -> dict:
     if not isinstance(obj, dict):
         raise ValueError("Object must be a dictionary")
     
-    return orjson.dumps({
-        "href": obj['url'],
-        "rel": obj['rel'],
-        "type": obj['type'],
-        "title": obj['title']
-    }).decode("utf-8")
+    href = obj.get('url', None)
+    if href is None:
+        href = obj.get('href', None)
+    if href is None:
+        raise ValueError("Object must have a 'url' or 'href' key")
     
-def generate_links(obj: list[dict]) -> str:
+    rel = obj.get('rel', None)
+    if rel is None:
+        raise ValueError("Object must have a 'rel' key")
+    
+    doc_type = obj.get('type', None)
+    if doc_type is None:
+        raise ValueError("Object must have a 'type' key")
+        
+    title = obj.get('title', None)
+    if title is None:
+        raise ValueError("Object must have a 'title' key")
+    
+    return {
+        "href": href,
+        "rel": rel,
+        "type": doc_type,
+        "title": title
+    }
+    
+def generate_links(obj: list[dict]) -> list[dict]:
     if not isinstance(obj, list):
         raise ValueError("Input must be a list of dictionaries")
     
-    return orjson.dumps([orjson.loads(generate_link(item)) for item in obj]).decode("utf-8")
+    return [generate_link(item) for item in obj]
