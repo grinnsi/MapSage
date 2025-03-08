e918a272c1bd4e1bef631b3890e62874232b0f12
grinnsi <anton.may@gmx.net>
2025-03-08 22:09:24 +0100

    Add Jinja2 template configuration to route_config.py to provide rendering templates

--- server/ogc_apis/route_config.py
+++ server/ogc_apis/route_config.py
@@ -1,9 +1,18 @@
 from enum import Enum
+from fastapi.templating import Jinja2Templates
+import jinja2
+from pydantic import Field
+from typing_extensions import Annotated
 
 from fastapi import Query
 
 API_ROUTE = "/api"
 FEATURES_ROUTE = "/features"
+TEMPLATE_RESPONSE = Jinja2Templates(directory="./../jinja_templates")
+TEMPLATE_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.PackageLoader("server", "jinja_templates"),
+    autoescape=jinja2.select_autoescape(),
+)
 
 class _ReturnFormat(str, Enum):
     json = "json"
