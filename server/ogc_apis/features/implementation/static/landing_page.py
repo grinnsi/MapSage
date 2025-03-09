9147db7064da10b840bde99bf8217f67e76480e8
grinnsi <anton.may@gmx.net>
2025-03-09 21:35:08 +0100

    Refactor landing page link generation to use pre_render_helper for better link creation and add HTML-links

--- server/ogc_apis/features/implementation/static/landing_page.py
+++ server/ogc_apis/features/implementation/static/landing_page.py
@@ -4,6 +4,7 @@ from server.database.db import Database
 from server.database.models import GeneralOption
 from server.ogc_apis.features.models.landing_page import LandingPage
 from server.ogc_apis.features.models.link import Link
+from server.ogc_apis.features.implementation import pre_render_helper
 
 
 def generate_landing_page_object(base_url: str) -> LandingPage:   
@@ -13,14 +14,40 @@ def generate_landing_page_object(base_url: str) -> LandingPage:
     domain = base_url.rstrip("/")
     features_base_route = f"{domain}{ogc_api_config.routes.FEATURES}"
     
-    known_links = [
-        Link(href=features_base_route, rel="self", type="application/json", title="This document"),
-        Link(href=f"{features_base_route}{ogc_api_config.routes.API}.json", rel="service-desc", type="application/vnd.oai.openapi+json;version=3.0", title="the API definition"),
-        Link(href=f"{features_base_route}{ogc_api_config.routes.API}.html", rel="service-doc", type="text/html", title="the API documentation"),
-        Link(href=f"{features_base_route}/conformance", rel="conformance", type="application/json", title="OGC API conformance classes implemented by this server"),
-        Link(href=f"{features_base_route}/collections", rel="data", type="application/json", title="Information about the feature collections"),
-    ]
+    link_self = {
+        "url": features_base_route + "/",
+        "rel": "self",
+        "title": "This document as {format_name}",
+    }
     
-    landing_page = LandingPage(title=title.value, description=description.value, links=known_links)
+    link_conformance = {
+        "url": f"{features_base_route}/conformance",
+        "rel": "conformance",
+        "title": "OGC API conformance classes implemented by this server as {format_name}",
+    }
+    
+    link_collections = {
+        "url": f"{features_base_route}/collections",
+        "rel": "data",
+        "title": "Information about the feature collections as {format_name}",
+    }
+    
+    links = pre_render_helper.generate_links([link_self, link_conformance, link_collections], multiple_types=True)
+    
+    links.append({
+        "href": f"{features_base_route}{ogc_api_config.routes.API}",
+        "rel": "service-desc",
+        "type": "application/vnd.oai.openapi+json;version=3.0",
+        "title": "The API definition",
+    })
+    
+    links.append({
+        "href": f"{features_base_route}{ogc_api_config.routes.API}?f=html",
+        "rel": "service-doc",
+        "type": "text/html",
+        "title": "The API documentation",
+    })
+    
+    landing_page = LandingPage(title=title.value, description=description.value, links=links)
     
     return landing_page
