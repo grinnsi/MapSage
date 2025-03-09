0a9b52494ae0b7ab456988d472634f0ee39b7e18
grinnsi <anton.may@gmx.net>
2025-03-09 12:11:33 +0100

    Add custom MIME type support for geojson in ReturnFormat class

--- server/ogc_apis/config/formats.py
+++ server/ogc_apis/config/formats.py
@@ -1,3 +1,4 @@
+import mimetypes
 from enum import Enum
 
 
@@ -8,3 +9,17 @@ class ReturnFormat(str, Enum):
     @classmethod
     def get_default(cls):
         return cls.json
+    
+    @classmethod
+    def get_custom_mimetypes(cls):
+        return [
+            {
+                "ext": "geojson",
+                "type": "application/geo+json",
+            },
+        ]
+
+mimetypes.init()
+
+for mimetype in ReturnFormat.get_custom_mimetypes():
+    mimetypes.add_type(type=mimetype["type"], ext="." + mimetype["ext"])
