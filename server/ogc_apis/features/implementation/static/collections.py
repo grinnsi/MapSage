4bc0ec22479f49337369cc53f8ab26c5098733ea
grinnsi <anton.may@gmx.net>
2025-03-10 12:00:11 +0100

    Refactor collections object generation to update CRS handling, replacing global CRS with specific CRS for each collection

--- server/ogc_apis/features/implementation/static/collections.py
+++ server/ogc_apis/features/implementation/static/collections.py
@@ -18,8 +18,12 @@ def generate_collections_object(collections_url: str) -> Collections:
     if len(all_collections) == 0:
         return Collections(links=links, collections=[])
     
-    # FIXME: Remove global crs from collections and replace with #/crs
-    shared_crs = set.intersection(*map(set, [collection.crs_json for collection in all_collections]))
+    shared_crs = list(set.intersection(*map(set, [collection.crs_json for collection in all_collections])))
+    for collection in all_collections:
+        specific_crs = [crs for crs in collection.crs_json if crs not in shared_crs]
+        specific_crs.insert(0, "#/crs")
+        collection.crs_json = specific_crs
+    
     all_collections_model = [collection.to_collection() for collection in all_collections]
     
-    return Collections(links=links, collections=all_collections_model, crs=list(shared_crs))
+    return Collections(links=links, collections=all_collections_model, crs=shared_crs)
