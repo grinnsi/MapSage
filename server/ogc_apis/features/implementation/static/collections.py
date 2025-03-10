eaf506dfbbccd657f82cc9b3afbf8f554f75921b
grinnsi <anton.may@gmx.net>
2025-03-10 22:02:06 +0100

    Add update_database_object function to update or insert collections json in db; update the collections json when creating a collection in dashboard

--- server/ogc_apis/features/implementation/static/collections.py
+++ server/ogc_apis/features/implementation/static/collections.py
@@ -1,8 +1,8 @@
-
-from server.database.db import Database
-from server.database.models import CollectionTable
+from server.database.db import Database, DatabaseSession
+from server.database.models import CollectionTable, PreRenderedJson
 from server.ogc_apis.features.implementation import pre_render_helper
 from server.ogc_apis.features.models.collections import Collections
+from server.ogc_apis import config
 
 def generate_object(collections_url: str) -> Collections:
     link_self = {
@@ -26,3 +26,23 @@ def generate_object(collections_url: str) -> Collections:
     all_collections_model = [collection.to_collection() for collection in all_collections]
     
     return Collections(links=links, collections=all_collections_model, crs=shared_crs)
+
+def update_database_object(collections_url: str = None, app_base_url: str = None) -> dict:
+    if collections_url is None and app_base_url is None:
+        raise ValueError("Either collections_url or base_url must be provided.")
+    
+    if collections_url is None:
+        app_base_url = app_base_url.lstrip("/")
+        collections_url = f"{app_base_url}{config.routes.FEATURES}/collections"
+    
+    with DatabaseSession() as session:
+        generated_collections = generate_object(collections_url)
+        pre_rendered_collections = PreRenderedJson(key="collections", json_value=generated_collections.model_dump_json(by_alias=True, exclude_unset=True, exclude_none=True))
+        collections_object = session.get(PreRenderedJson, pre_rendered_collections.key)
+        if collections_object:
+            collections_object.sqlmodel_update(pre_rendered_collections)
+        session.add(collections_object)
+        session.commit()
+        session.refresh(collections_object)
+
+    return pre_rendered_collections.json_value
