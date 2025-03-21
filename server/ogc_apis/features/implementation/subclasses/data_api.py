375b6a9acd204f39ba7a47f4dc1327c475461eca
grinnsi <anton.may@gmx.net>
2025-03-21 19:32:39 +0100

    Implementation of get_features and everything else (too lazy to list everything)

--- server/ogc_apis/features/implementation/subclasses/data_api.py
+++ server/ogc_apis/features/implementation/subclasses/data_api.py
@@ -89,4 +89,84 @@ class DataApi(BaseDataApi):
         session: sqlmodel.Session,
     ) -> FeatureCollectionGeoJSON:
         """Fetch features of the feature collection with id `collectionId`.  Every feature in a dataset belongs to a collection. A dataset may consist of multiple feature collections. A feature collection is often a collection of features of a similar type, based on a common schema.  Use content negotiation to request HTML or GeoJSON."""
-        ...
+        
+        collections: list[models.CollectionTable] = dynamic.collection_impl.get_collection_by_id(id=collectionId, session=session)
+        if len(collections) == 0:
+            raise HTTPException(status_code=404, detail="The requested resource does not exist on the server. For example, a path parameter had an incorrect value.")
+        collection = collections[0]
+        
+        if crs is None:
+            crs = "http://www.opengis.net/def/crs/OGC/1.3/CRS84" if not collection.is_3D else "http://www.opengis.net/def/crs/OGC/0/CRS84h"
+        
+        if crs not in collection.crs_json:
+            raise HTTPException(status_code=400, detail="The requested CRS is not applicable to this collection. List of supported CRSs: " + ", ".join(collection.crs_json))
+        
+        if bbox_crs is not None and bbox_crs not in collection.crs_json:
+            raise HTTPException(status_code=400, detail="The BBOX-CRS is not applicable to this collection. List of supported BBOX-CRSs: " + ", ".join(collection.crs_json))
+            
+        if bbox_crs is None and bbox is not None:
+            bbox_crs = "http://www.opengis.net/def/crs/OGC/1.3/CRS84" if not collection.is_3D else "http://www.opengis.net/def/crs/OGC/0/CRS84h"
+        
+        time_interval = None
+        if datetime is not None:
+            parts = datetime.split("/")
+            try:
+                if len(parts) == 1:
+                    datetime = dt.datetime.fromisoformat(datetime)
+                    time_interval = (datetime, datetime)
+                elif len(parts) == 2:
+                    if parts[0] == "" or parts[0] == "..":
+                        time_interval = (None, dt.datetime.fromisoformat(parts[1]))
+                    elif parts[1] == "" or parts[1] == "..":
+                        time_interval = (dt.datetime.fromisoformat(parts[0]), None)
+                    else:
+                        time_interval = (dt.datetime.fromisoformat(parts[0]), dt.datetime.fromisoformat(parts[1]))
+                else:
+                    raise ValueError("Too many datetime parts")
+            except ValueError as error:
+                raise HTTPException(status_code=400, detail="The datetime parameter is not in the correct format. It must adhere to RFC 3339 and the OGC specification.") from error
+
+        dataset_wrapper = gdal_utils.get_dataset_from_collection_table(collection, session)
+        
+        try:
+            features, total_feature_count, returned_feature_count = dynamic.feature_impl.get_features(dataset_wrapper, collection.layer_name, limit, offset, bbox, bbox_crs, crs)
+        except ValueError as error:
+            raise HTTPException(status_code=400, detail=str(error)) from error
+        
+        next_page = returned_feature_count + offset < total_feature_count
+
+        cur_url = request.url.remove_query_params("f")
+        cur_url = cur_url.include_query_params(limit=limit, offset=offset)
+        next_url = cur_url.include_query_params(offset=min(offset + limit, total_feature_count - 1))._url if next_page else None
+        prev_url = cur_url.include_query_params(offset=max(offset - limit, 0))._url if offset > 0 else None
+        
+        links = dynamic.feature_impl.generate_features_links(request.base_url._url, cur_url._url, next_url, prev_url)
+        
+        if "crs" in features:
+            del features["crs"]
+        
+        features["timeStamp"] = dt.datetime.now().replace(microsecond=0).isoformat()
+        features["numberMatched"] = total_feature_count
+        features["numberReturned"] = returned_feature_count
+        features["links"] = links
+        
+        headers = {
+            "Content-Crs": "<" + crs + ">",
+            "Cache-Control": "max-age=60",
+        }
+        if format == ogc_api_config.ReturnFormat.html:
+            return ogc_api_config.templates.response("features.html",
+                request=request,
+                context={
+                    "features": features,
+                    "collection": collection,
+                    "crs_wkt": gdal_utils.get_wkt_from_uri(crs),
+                },
+                headers=headers
+            )
+
+        return ogc_api_config.formats.GeoJSONResponse(
+            status_code=200,
+            content=features, 
+            headers=headers,
+        )
