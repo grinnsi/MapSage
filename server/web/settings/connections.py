f2a0796132077504f1f72f07ce37061e5973d203
grinnsi <anton.may@gmx.net>
2025-03-23 21:45:53 +0100

    Refactor connections.py to use models.Dataset for consistency and sort all connections by name

--- server/web/settings/connections.py
+++ server/web/settings/connections.py
@@ -1,13 +1,13 @@
 from typing import Union
 from flask import Response
 
-from server.database.models import Dataset
+from server.database import models
 from server.database.db import Database
 
 def get_connections() -> Response:
     """Gets all connections to a (PostgreSQL) database from the internal database and returns a Response with the data and status"""
     
-    results: Union[list[Dataset], None] = Database.select_sqlite_db(table_model=Dataset, select_all=True)
+    results: Union[list[models.Dataset], None] = Database.select_sqlite_db(table_model=models.Dataset, select_all=True)
 
     if not results:
         results = []
@@ -18,6 +18,7 @@ def get_connections() -> Response:
         dataset_dict = dataset.to_dict()
         json_data.append(dataset_dict)
 
+    json_data.sort(key=lambda x: x["name"])
     return json_data
 
 def create_new_connection(form: dict) -> Response:
@@ -26,8 +27,8 @@ def create_new_connection(form: dict) -> Response:
     test_successful = Database.test_pg_connection(form)
     
     if test_successful:
-        form["type"] = Dataset.Type.DB
-        new_connection: Dataset = Dataset.from_dict(form)       
+        form["type"] = models.Dataset.Type.DB
+        new_connection: models.Dataset = models.Dataset.from_dict(form)       
         new_connection = Database.insert_sqlite_db(data_object=new_connection)
         if new_connection is None:
             return Response(status=500, response="Error while inserting connection into database")
@@ -39,7 +40,7 @@ def create_new_connection(form: dict) -> Response:
 def delete_connection(form: dict) -> Response:
     """Deletes a connection from the internal database"""
     
-    connection: Union[None, Dataset] = Database.delete_sqlite_db(table_model=Dataset, primary_key_value=form["uuid"])
+    connection: Union[None, models.Dataset] = Database.delete_sqlite_db(table_model=models.Dataset, primary_key_value=form["uuid"])
     if not connection:
         return Response(status=404, response="Connection not found")
     
